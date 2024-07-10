79983609729f2cce9987fdbf829703b77923af45
Ryan Mingun Choi <mgc202077@gmail.com>
2024-07-10 00:58:08 -0400

    add domain&database

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,5 @@
+-- products
+INSERT INTO products VALUES ('1', 'Iced Americano', 'Ice cold', 3.5);
+INSERT INTO products VALUES ('2', 'Americano', 'Hot', 3.0)
+INSERT INTO products VALUES ('3', 'Iced Latte', 'Ice', 5.5)
+INSERT INTO products VALUES ('4', ' Latte', 'Hot', 5.0)
